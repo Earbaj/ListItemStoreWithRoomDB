4d3243cc86d72c732b9113b883e31db6c0ca8a29
earbaj <earbajsaria3@gmail.com>
2022-10-20 12:10:12 +0600

    Implement navigation controller to mainactivity

--- app/src/main/java/com/example/inventorylistapp/ui/ListItemFragment.kt
+++ app/src/main/java/com/example/inventorylistapp/ui/ListItemFragment.kt
@@ -5,9 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavController
 import com.example.inventorylistapp.R
 
 class ListItemFragment : Fragment() {
+
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
