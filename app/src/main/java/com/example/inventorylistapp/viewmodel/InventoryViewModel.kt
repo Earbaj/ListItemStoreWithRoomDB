d19d0d5819c18d55904e0d6c0c1b113c0e06f61c
earbaj <earbajsaria3@gmail.com>
2022-10-20 11:53:03 +0600

    Add viewmodel and viewmodel factory class and implement methodes

---
+++ app/src/main/java/com/example/inventorylistapp/viewmodel/InventoryViewModel.kt
@@ -0,0 +1,29 @@
+package com.example.inventorylistapp.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.inventorylistapp.data.Item
+import com.example.inventorylistapp.data.ItemDao
+import kotlinx.coroutines.launch
+
+class InventoryViewModel(private val itemDao: ItemDao):ViewModel() {
+
+    private fun insertData(item: Item){
+        viewModelScope.launch {
+            itemDao.insert(item)
+        }
+    }
+
+    private fun getNewItem(itemName: String, itemPrice: String, itemQuantity: String): Item{
+        return Item(
+            itemName = itemName,
+            itemPrice = itemPrice.toDouble(),
+            quantityInStock = itemQuantity.toInt()
+        )
+    }
+
+    fun addNewItem(itemName: String, itemPrice: String, itemQuantity: String){
+        val item = getNewItem(itemName,itemPrice,itemQuantity)
+        insertData(item)
+    }
+}
